64d68fbcaa4e20a666fccffebf8034bdd45e08ed
SageCT <egas6554@gmail.com>
2023-09-04 13:17:57 -0500

    p5.cpp passes

--- hw/p5/p5.cpp
+++ hw/p5/p5.cpp
@@ -2,40 +2,89 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <unordered_map>
 #include <vector>
+
 using namespace std;
 
 struct node {
   int val;
-  node *next;
+  node *left, *right;
+
+  node(int v) : val(v), left(nullptr), right(nullptr) {}
 };
 
-vector<int> getPostOrder(int n, vector<int> preO, vector<int> postO) {}
+node *buildTreeHelper(vector<int> &preO, vector<int> &inO, int inStart,
+                      int inEnd, int &preIndex,
+                      unordered_map<int, int> &inorderMap) {
+  if (inStart > inEnd)
+    return nullptr;
+
+  node *root = new node(preO[preIndex++]);
+  int inIndex = inorderMap[root->val];
+  root->left =
+      buildTreeHelper(preO, inO, inStart, inIndex - 1, preIndex, inorderMap);
+  root->right =
+      buildTreeHelper(preO, inO, inIndex + 1, inEnd, preIndex, inorderMap);
+
+  return root;
+}
+
+node *buildTree(vector<int> preO, vector<int> inO) {
+  int preIndex = 0;
+
+  unordered_map<int, int> inorderMap;
+  for (int i : inO)
+    inorderMap[inO[i]] = i;
+
+  return buildTreeHelper(preO, inO, 0, inO.size() - 1, preIndex, inorderMap);
+}
+
+void buildPostOrder(node *root, vector<int> &v) {
+  if (root) {
+    buildPostOrder(root->left, v);
+    buildPostOrder(root->right, v);
+    v.push_back(root->val);
+  }
+}
 
 int main() {
   //* Testing Inputs
-  int N = 0;
-  vector<int> preOrder = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  vector<int> inOrder = {3, 2, 5, 4, 6, 1, 8, 7, 9};
+  // vector<int> preOrder = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  // vector<int> inOrder = {3, 2, 5, 4, 6, 1, 8, 7, 9};
+  // int N = preOrder.size();
 
-  // // Inputs from cin
-  // vector<int> pre, post;
-  // string line;
-  // stringstream ss;
-  // int c;
-  // getline(cin, line);
-  // line.clear();
+  // Inputs from cin
+  vector<int> preOrder, inOrder;
+  string line;
+  stringstream ss;
+  int c;
+  getline(cin, line);
+  line.clear();
 
-  // // Inputs for pre
-  // getline(cin, line);
-  // ss << line;
-  // while (ss >> c)
-  //   pre.push_back(c);
+  // Inputs for pre
+  getline(cin, line);
+  ss << line;
+  while (ss >> c)
+    preOrder.push_back(c);
+  line.clear();
+  ss.clear();
 
-  // // Inputs for post
-  // line.clear();
-  // getline(cin, line);
-  // ss << line;
-  // while (ss >> c)
-  //   post.push_back(c);
+  // Inputs for post
+  getline(cin, line);
+  ss << line;
+  while (ss >> c)
+    inOrder.push_back(c);
+
+  int N = preOrder.size();
+  node *root = buildTree(preOrder, inOrder);
+  vector<int> res;
+  buildPostOrder(root, res);
+
+  for (int i = 0; i < N; i++) {
+    if (i < N - 1)
+      cout << res[i] << " ";
+    else
+      cout << res[i];
+  }
 }
---
+++ hw/p5/submissionID_p5.md
@@ -0,0 +1,2 @@
+(cpp) 8/8 passed. First try lets go!
+bdbe3da8-3704-47e4-abf8-9411026f4e4f
