aacbda735a7ca190183aa41e80014bbac2a6fe77
SageCT <egas6554@gmail.com>
2023-09-07 16:47:11 -0500

    p12 passes all test cases

--- hw/p12/p12.cpp
+++ hw/p12/p12.cpp
@@ -6,23 +6,28 @@
 #include <vector>
 using namespace std;
 
-void divideArrHelper(vector<int> &arr, int k, int startIdx, int n) {
-  if (startIdx + k > arr.size() || n == k)
-    return;
+bool divideArrHelper(vector<int> &arr, int k, int startIdx) {
+  // if startIdx == end of the array, return
+  if (arr.size() == 0)
+    return true;
 
-  // Add the subarray of size k starting from the current index
-  auto minElementIndex = std::min_element(arr.begin(), arr.end());
-  int minElement = *minElementIndex;
-  arr.erase(minElementIndex);
+  //* Find the smallest element in the array and erase it
+  int minNum = *min_element(arr.begin(), arr.end());
+  arr.erase(min_element(arr.begin(), arr.end()));
 
-  return (minElement == n) ? divideArrHelper(arr, k, startIdx, n + 1) : false;
+  // Find the smallest element of
+  for (int i = startIdx + 1; i < startIdx + k; i++) {
 
+    if (find(arr.begin(), arr.end(), ++minNum) != arr.end()) {
+      arr.erase(find(arr.begin(), arr.end(), minNum));
+    } else
+      return false;
+  }
   // Recursively call the function for the next subarray
-  divideArrHelper(arr, k, startIdx + k, n);
+  return divideArrHelper(arr, k, startIdx + k);
 }
 
 bool divideArr(vector<int> &arr, int k, int startIdx) {
-
   sort(arr.begin(), arr.end());
 
   if (arr.size() % k > 0 || k == 0) {
@@ -30,23 +35,26 @@ bool divideArr(vector<int> &arr, int k, int startIdx) {
     return false;
   }
 
-  else {
-    for (int i = 0; i < arr.size() / k; i++)
-      divideArrHelper(arr, k, startIdx + (k * i), arr[0]);
-    return true;
+  if (!divideArrHelper(arr, k, startIdx)) {
+    cout << "false";
+    return false;
   }
+  cout << "true";
+  return true;
 }
 
 int main() {
   stringstream ss;
   vector<int> arr;
-  vector<vector<int>> res;
   string line;
   int c, divSize;
 
   // //* Testing Values
-  // arr = {1, 2, 3, 3, 4, 5, 5, 6};
-  // divSize = 4;
+  // vector<int> arr = {1, 2, 3, 3, 4, 5, 5, 6};
+  // int divSize = 4;
+
+  // arr = {1, 2, 2, 3};
+  // divSize = 2;
 
   // * Input Handling
   getline(cin, line);
@@ -62,6 +70,6 @@ int main() {
   ss >> c;
   divSize = c;
 
-  if (divideArr(arr, divSize, 0))
-    ;
+  // * Calling the functions
+  divideArr(arr, divSize, 0);
 }
--- hw/p12/submissionID_p12.md
+++ hw/p12/submissionID_p12.md
@@ -1,2 +1,5 @@
 (cpp) passed 5/8.
 c58cabc3-10c8-4e3b-881b-fa7a95ed2d3f
+
+(cpp) passed 8/8!
+48ac7f70-24de-4efb-865d-bc812ac69683
