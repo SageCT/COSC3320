fe0cab5e1ccae9c7498873b92e475167a1be6e78
SageCT <egas6554@gmail.com>
2023-09-19 16:01:25 -0500

    p19 mostly done, outputting 0s

--- hw/p19/p19.cpp
+++ hw/p19/p19.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 using namespace std;
 
+int robPts = 0, racPts = 0;
+
 int helper(vector<int> &arr1, vector<int> &arr2) {
   int index = 0, tempScore = 0, finalScore = 0;
   while (index < arr2.size() - 1) {
@@ -27,25 +29,75 @@ void merge(vector<int> &robArr, vector<int> &racArr, int left, int mid,
 
   // Rob Arr Left and Right
   for (int i = 0; i < lMid; i++)
-    robArrLeft.push_back(robArr[i]);
-  for (int i = 0; i < lMid; i++)
-    robArrRight.push_back(robArr[mid + i + 1]);
+    robArrLeft.push_back(robArr[i]), racArrLeft.push_back(robArr[mid + i + 1]);
 
   // Rac Arr Left and Right
   for (int i = 0; i < rMid; i++)
-    racArrLeft.push_back(robArr[i]);
-  for (int i = 0; i < rMid; i++)
-    racArrRight.push_back(robArr[mid + i + 1]);
+    robArrRight.push_back(robArr[i]),
+        racArrRight.push_back(robArr[mid + i + 1]);
 
-  int robi, robj;
-  int raci, racj;
+  int robi = 0, robj = 0;
+  int raci = 0, racj = 0;
+
+  if (robi < robArrLeft.size() && robj < robArrRight.size())
+    robPts += helper(racArrLeft, robArrRight);
+
+  if (raci < racArrLeft.size() && racj < racArrRight.size())
+    racPts += helper(racArrLeft, robArrRight);
+
+  robi = 0, robj = 0;
+  raci = 0, racj = 0;
+  int robIndex = 0, racIndex = 0;
+
+  // ? n1 = lMid n2 = rMid
+  // ? rob merge section
+  while (robi < lMid && robj < rMid) {
+    if (robArrLeft[robi] <= robArrRight[robj]) {
+      robArr[robIndex] = robArrLeft[robi++];
+    } else {
+      robArr[robIndex++] = robArrRight[robj++];
+    }
+  }
+
+  while (robi < lMid)
+    robArr[robIndex++] = robArrLeft[robi++];
+
+  while (robj < rMid)
+    robArr[robIndex++] = robArrRight[robj++];
+  // ? n1 = lMid n2 =
+  // ? rac merge section
+
+  while (raci < lMid && racj < rMid) {
+    if (racArrLeft[robi] <= racArrRight[racj]) {
+      racArr[racIndex] = racArrLeft[raci++];
+    } else {
+      racArr[racIndex] = racArrRight[racj++];
+    }
+    racIndex++;
+  }
+
+  while (raci < lMid)
+    racArr[racIndex++] = racArrLeft[raci++];
+
+  while (racj < rMid)
+    racArr[racIndex++] = racArrRight[racj++];
 }
 
-void mergeSort(vector<int> &arr1, vector<int> &arr2, int left, int right) {}
+void mergeSort(vector<int> &arr1, vector<int> &arr2, int left, int right) {
+
+  if (left < right) {
+    int mid = left + (right - left) / 2;
+
+    mergeSort(arr1, arr2, left, mid);
+    mergeSort(arr1, arr2, mid + 1, right);
+
+    merge(arr1, arr2, left, mid, right);
+  }
+}
 
 int main() {
   vector<int> robArr, racArr;
-  int a, robPts, rackPts;
+  int a;
   string line = "";
   stringstream ss;
 
@@ -60,4 +112,7 @@ int main() {
   ss << line;
   while (ss >> a)
     racArr.push_back(a);
+
+  mergeSort(robArr, racArr, 0, robArr.size());
+  cout << robPts << " " << racPts;
 }
