42e93c0d66ad5c1a3e9968c2e2a460f587dc027e
SageCT <99381480+SageCT@users.noreply.github.com>
2023-10-25 22:49:40 -0500

    Finished p6.py

--- hw/p6/p6.py
+++ hw/p6/p6.py
@@ -1,26 +1,36 @@
-def numIslands(grid):
-    if not grid:
+def numIslands(grid) -> int:
+    if not grid or not grid[0]:
         return 0
 
-    def dfs(i, j):
-        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == "0":
+    islands = 0
+    visit = set()
+    rows, cols = len(grid), len(grid[0])
+
+    def dfs(r, c):
+        if (
+            r not in range(rows)
+            or c not in range(cols)
+            or grid[r][c] == "0"
+            or (r, c) in visit
+        ):
             return
-        grid[i][j] = "0"  # Mark the current land as visited
-        # Check neighboring cells
-        dfs(i - 1, j)
-        dfs(i + 1, j)
-        dfs(i, j - 1)
-        dfs(i, j + 1)
 
-    count = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == "1":
-                count += 1
-                dfs(i, j)
+        visit.add((r, c))
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        for dr, dc in directions:
+            dfs(r + dr, c + dc)
 
-    return count
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == "1" and (r, c) not in visit:
+                islands += 1
+                dfs(r, c)
+    return islands
 
 
 lines = int(input())
-inputs = [input() in range(lines)]
+inputs = [str(input()).strip().split(" ") for _ in range(lines)]
+
+# print(inputs)
+
+print(numIslands(inputs))
---
+++ hw/p6/submissionID_p6.md
@@ -0,0 +1,2 @@
+(python) 8/8 Test Cases Passed.
+67121b45-da46-4881-99f8-216a32df0329
